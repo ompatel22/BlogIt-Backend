ebadb529c4e7b94d8db9507151b33269d5ee52ed
Om Patel <omupatel22@gmail.com>
2025-02-17 20:22:44 +0530

    added dependencies

---
+++ src/main/java/com/spring/blogitbackend/BlogItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.blogitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogItBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/blogitbackend/BlogItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
